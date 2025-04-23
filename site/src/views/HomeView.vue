a897d42bdd2b4fa163338fee5ef685121b44f07d
Yuval Greenfield <ubershmekel@gmail.com>
2025-04-23 00:03:43 -0700

    Show error messages and TBD dates

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -4,17 +4,31 @@ import yaml from 'js-yaml'
 import type { PageYaml, Release } from '@/types/yamls'
 
 const yamlData = ref<PageYaml | null>(null)
+const errorMessage = ref<string | null>(null)
 
 onMounted(async () => {
   const res = await fetch('/data/games/mario.yml')
+  // const res = await fetch('/data/books/game-of-thrones.yml')
   const text = await res.text()
   const data = yaml.load(text) as PageYaml;
   console.log(data);
+  if (res.status !== 200) {
+    errorMessage.value = `Error fetching data: ${res.statusText}`
+    return
+  }
+  if (!data.releases) {
+    errorMessage.value = 'Failed to fetch releases'
+    return
+  }
   yamlData.value = data;
 })
 
 function simpleDate(date: Date) {
-  return date.toISOString().split('T')[0]
+  if (isValidDate(date)) {
+    return date.toISOString().split('T')[0]
+  } else {
+    return date;
+  }
 }
 
 function monthsBetween(dateA: Date, dateB: Date) {
@@ -32,36 +46,53 @@ function yearsBetween(dateA: Date, dateB: Date) {
 }
 
 function averageReleaseGap(releases: Release[]) {
-  const dates = releases.map(release => release.release_date)
+  const dates = releases.filter(release => isValidDate(release.release_date)).map(release => release.release_date)
+  dates.push(new Date());
   const sortedDates = dates.sort((a, b) => a.getTime() - b.getTime())
   const firstDate = sortedDates[0]
   const lastDate = sortedDates[sortedDates.length - 1]
   return yearsBetween(firstDate, lastDate) / (releases.length - 1);
 }
 
+function isValidDate(value: unknown): value is Date {
+  return value instanceof Date && !isNaN(value.getTime());
+}
+
+function recentRelease(releases: Release[]): Release | undefined {
+  for (const release of releases) {
+    if (isValidDate(release.release_date)) {
+      return release
+    }
+  }
+}
+
 </script>
 
 <template>
   <main>
+    <div v-if="errorMessage">
+      {{ errorMessage }}
+    </div>
+    <p v-if="!yamlData && !errorMessage">Loading...</p>
     <div v-if="yamlData">
-      <h2>{{ yamlData.name }} release date data</h2>
+      <h2>{{ yamlData.name }} release dates</h2>
 
       <p>Creator: {{ yamlData.company }}</p>
-      <p>Since last release: <span class="years-between">{{ yearsBetween(new Date(),
-        yamlData.releases[0].release_date).toFixed(1) }} years</span></p>
       <p>Average years between releases: <span class="years-between">{{ averageReleaseGap(yamlData.releases).toFixed(1)
-          }} years</span></p>
+      }} years</span></p>
       <p>Releases:</p>
       <ul>
+        <li>Since last release: <span class="years-between">{{ yearsBetween(new Date(),
+          recentRelease(yamlData.releases)!.release_date).toFixed(1) }} years</span></li>
         <li v-for="(release, index) in yamlData.releases" :key="release.title">
           <p>{{ simpleDate(release.release_date) }} <span class="title">{{ release.title }}</span></p>
-          <p class="years-between" v-if="index + 1 < yamlData.releases.length">{{ yearsBetween(
-            yamlData.releases[index + 1].release_date, release.release_date).toFixed(1) }} years</p>
+          <p class="years-between" v-if="index + 1 < yamlData.releases.length && isValidDate(release.release_date)">{{
+            yearsBetween(
+              yamlData.releases[index + 1].release_date, release.release_date).toFixed(1) }} years</p>
           <p></p>
         </li>
       </ul>
     </div>
-    <p v-else>Loading...</p>
 
   </main>
 </template>
