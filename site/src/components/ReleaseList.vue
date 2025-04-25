d7ec1c7b23ea6a6e56913f99fdf787c6d6eed61b
Yuval Greenfield <ubershmekel@gmail.com>
2025-04-25 08:44:46 -0700

    Show when a release is in the future

--- site/src/components/ReleaseList.vue
+++ site/src/components/ReleaseList.vue
@@ -8,7 +8,9 @@
       <GapBar :gap="gapByIndex(-1)" :max-gap="maxGap"></GapBar>
     </li>
     <li v-for="(release, index) in releases" :key="release.title">
-      <p>{{ simpleDate(release.release_date) }} <span class="title">{{ release.title }}</span></p>
+      <p>{{ simpleDate(release.release_date) }} <span class="title">{{ release.title }}</span>
+        <span v-if="release.release_date > new Date()" class="future-release"> (in the future)</span>
+      </p>
       <GapBar v-if="hasGap(index)" :gap="gapByIndex(index)" :maxGap="maxGap"></GapBar>
     </li>
   </ul>
@@ -21,15 +23,6 @@ import { average, isValidDate, simpleDate, yearsBetween } from '@/utils/dates';
 import GapBar from './GapBar.vue';
 
 const props = defineProps(['releases'])
-
-function averageReleaseGap(releases: Release[]) {
-  const dates = releases.filter(release => isValidDate(release.release_date)).map(release => release.release_date)
-  dates.push(new Date());
-  const sortedDates = dates.sort((a, b) => a.getTime() - b.getTime())
-  const firstDate = sortedDates[0]
-  const lastDate = sortedDates[sortedDates.length - 1]
-  return yearsBetween(firstDate, lastDate) / (releases.length - 1);
-}
 
 function recentRelease(releases: Release[]): Release | undefined {
   for (const release of releases) {
@@ -76,5 +69,9 @@ hr {
 
 ul {
   padding: 0;
+}
+
+.future-release {
+  color: rgb(242, 0, 193);
 }
 </style>
