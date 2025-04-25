a090e6ed1b1f69d29502511d5d05b4dae5fe6ada
Yuval Greenfield <ubershmekel@gmail.com>
2025-04-25 09:56:12 -0700

    Add logo to nav bar

--- site/src/App.vue
+++ site/src/App.vue
@@ -10,7 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
-        <RouterLink to="/">Release Date DB</RouterLink>
+        <RouterLink to="/"><img alt="Release Date DB" class="logo" src="/favicon.png" />Release Date DB</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
       <hr />
@@ -18,10 +18,42 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+
+  <footer>
+    <p>Release Date DB was made by <a href="https://bsky.app/profile/ubershmekel.bsky.social">ubershmekel</a> and <a
+        href="https://github.com/ubershmekel/release-date-db">the source is on github</a></p>
+  </footer>
 </template>
 
 <style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  /* This centers the content vertically */
+}
+
 nav a {
   margin-right: 16px;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  /* Center the image and text together */
+
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+nav a:visited {
+  color: inherit;
+}
+
+.logo {
+  height: 2rem
+}
+
+footer {
+  margin-top: 3rem;
 }
 </style>
