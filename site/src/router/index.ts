a21ab50ee71dc51c4fa530c0934426fdae5ccaad
Yuval Greenfield <ubershmekel@gmail.com>
2025-04-25 09:33:02 -0700

    Fix netlify 404

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectView from '@/views/ProjectView.vue'
 import AboutView from '@/views/AboutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => ProjectView,
+    },
+    {
+      path: '/:pathMatch(.*)*', // 👈 Catch-all route
+      name: 'NotFound',
+      component: NotFoundView,
     },
   ],
 })
---
+++ site/src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h2>404 not found</h2>
+    <p>Sorry, the page you visited seems to not exist.</p>
+  </main>
+</template>
+
+<style scoped></style>
